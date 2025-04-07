2e9b9663599cc18a8c78f241d659a34dd3901ec5
Vladislav Vasilev <81014877+faust403@users.noreply.github.com>
2025-04-07 18:19:55 +0200

    add custom token

--- contracts/spdBTC/spdBTC.sol
+++ contracts/spdBTC/spdBTC.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.28;
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IspdBTC.sol";
 
 /**
