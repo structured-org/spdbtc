eb34a077189bce1cf8c011334ec0d3892d13a985
Murad Karammaev <muradkarammaev@gmail.com>
2025-05-20 08:43:23 +0300

    feat: update deploy script

--- deployments/deploy.ts
+++ deployments/deploy.ts
@@ -1,29 +1,67 @@
 import { ethers } from 'hardhat';
+import { FunctionFragment } from 'ethers';
 
 require('dotenv').config({ path: __dirname + '.env' });
-const { CUSTODIAN } = process.env;
+if (!process.env.OWNER) {
+  throw new Error('OWNER must be specified');
+}
+const OWNER = process.env.OWNER!;
+if (!process.env.CUSTODIAN) {
+  throw new Error('CUSTODIAN must be specified');
+}
+const CUSTODIAN = process.env.CUSTODIAN!;
+const MAX_DEPOSIT = process.env.MAX_DEPOSIT ?? String(Math.pow(2, 52));
+
+const contracts: {
+    tokenMinter?: any;
+    spdBtc?: any;
+  } = {};
 
 async function main() {
-  const spdBtc = await ethers.getContractFactory('spdBTC');
-  const tokenMinter = await ethers.getContractFactory('TokenMinter');
-  const token_Minter = await tokenMinter.deploy();
-  await token_Minter.waitForDeployment();
+  const TokenMinter = await ethers.getContractFactory('TokenMinter');
+  contracts.tokenMinter = await TokenMinter.deploy();
+  await contracts.tokenMinter.waitForDeployment();
   console.log(
     "Contract's address (token_minter):",
-    await token_Minter.getAddress(),
+    await contracts.tokenMinter.getAddress(),
   );
-  const spd_Btc = await spdBtc.deploy(
-    await token_Minter.getAddress(),
-    'spdBTC',
-    'spdBTC',
+
+  const spdBTC = await ethers.getContractFactory('SpdBTC');
+  const ossifiableProxy = await ethers.getContractFactory('OssifiableProxy');
+  const spdBtcImplementation = await spdBTC.deploy();
+  await spdBtcImplementation.waitForDeployment();
+  console.log(
+    "Contract's address (spd_btc_implementation):",
+    await spdBtcImplementation.getAddress(),
   );
-  await spd_Btc.waitForDeployment();
-  console.log("Contract's address (spd_btc):", await spd_Btc.getAddress());
-  await spd_Btc.initializeProduct({
-    minDeposit: 0,
-    maxDeposit: BigInt(2 ** 52),
-    custodian: CUSTODIAN,
-  });
+
+  const initializeProductFunction = (
+    spdBTC.interface.fragments.filter(
+      (f) => f.type == 'function',
+    ) as FunctionFragment[]
+  ).find((f) => f.name == 'initializeProduct') as FunctionFragment;
+  const initializeProductData = spdBTC.interface.encodeFunctionData(
+    initializeProductFunction,
+    [
+      {
+        asset: await contracts.tokenMinter.getAddress(),
+        name: 'spdBTC',
+        symbol: 'spdBTC',
+        maxDeposit: MAX_DEPOSIT,
+        custodian: CUSTODIAN,
+      },
+    ],
+  );
+  const ossifiableProxyImplementation = await ossifiableProxy.deploy(
+    await spdBtcImplementation.getAddress(),
+    OWNER,
+    initializeProductData,
+  );
+  await ossifiableProxyImplementation.waitForDeployment();
+  contracts.spdBtc = spdBTC.attach(
+    await ossifiableProxyImplementation.getAddress(),
+  );
+  console.log("Contract's address (spd_btc):", await contracts.spdBtc.getAddress());
 }
 
 main()
