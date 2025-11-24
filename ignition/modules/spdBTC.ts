596a0ae8fa4f5d8a915a44180a93f424ca37812f
Murad Karammaev <muradkarammaev@gmail.com>
2025-11-24 18:34:50 +0200

    chore: migrate to hardhat 3

--- ignition/modules/spdBTC.ts
+++ ignition/modules/spdBTC.ts
@@ -1,13 +1,31 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
-export default buildModule('spdBTCModule', (m) => {
-  const asset = m.getParameter('asset');
-  const name = m.getParameter('name', 'spdBTC');
-  const symbol = m.getParameter('symbol', 'spdBTC');
+export default buildModule('spdBTC', (m) => {
+  const impl = m.getParameter("spdbtc_implementation");
+  const owner = m.getParameter("owner");
+  // defaults to WBTC on Ethereum
+  const asset = m.getParameter("asset", "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599");
+  const name = m.getParameter("name", "spdBTC");
+  const symbol = m.getParameter("symbol", "spdBTC");
+  const maxDeposit = m.getParameter("max_deposit", String(Math.pow(2, 52)));
+  const custodian = m.getParameter("custodian");
 
-  const spd = m.contract('spdBTC', [asset, name, symbol]);
-  return { spd };
+  // XXX: The next line deploys a contract. I have no idea how to avoid that.
+  //      I need to use this contract's ABI in this script though.
+  const spdBTC = m.contract("SpdBTC");
+  const initializeProductData = m.encodeFunctionCall(spdBTC, "initializeProduct",
+    [
+      {
+        asset,
+        name,
+        symbol,
+        maxDeposit,
+        custodian,
+      }
+    ],
+  );
+
+  const proxy = m.contract('OssifiableProxy', [impl, owner, initializeProductData]);
+
+  return { proxy };
 });
